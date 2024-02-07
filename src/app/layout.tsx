b64d1a575a08b7e2b44e9b067fc1bc53a1cb3b13
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2024-02-07 08:15:31 +0300

    fix: удаляет неиспользуемые импорты

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/styles/style.scss'
-import Navbar from '@/components/navbar/Navbar'
-import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,9 +18,8 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={inter.className}>
 				<div className='container'>
-					<Navbar />
 					{children}
-					<Footer />
+
 				</div>
 			</body>
 		</html>
