47e6c8e7b46085c6df2e72d550dd72acc0cd9aa8
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2024-02-08 10:06:08 +0300

    feat: стилизует интро главной страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
-const Home: React.FC = () => {
-	return <div>Homepage</div>
-}
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./home.module.scss";
 
-export default Home
+const Home: React.FC = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.description}>
+          Welcome to Creative Thoughts Agency, where innovative ideas come to
+          life. We believe in the power of creativity to transform businesses
+          and captivate audiences.
+        </p>
+        <div className={styles.buttons}>
+          <Link href={"#"} className={`${styles.button} ${styles.button_blue}`}>
+            Learn more
+          </Link>
+          <Link href={"#"} className={styles.button}>
+            Contact
+          </Link>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src={"/img/brands.png"}
+            alt=""
+            fill
+            className={styles.brandImg}
+          />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image
+          src={"/img/hero.gif"}
+          alt={" "}
+          width={500}
+          height={500}
+          className={styles.heroImg}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
