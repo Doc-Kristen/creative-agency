553d787c6dc6bfa7f01adcb26ae572f79340bf19
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2024-02-12 18:19:12 +0300

    feat: реализует функции добавления и удаления записей

--- src/components/post-form/PostForm.tsx
+++ src/components/post-form/PostForm.tsx
@@ -1,15 +1,18 @@
-import React from "react";
-import styles from './post-form.module.scss'
+"use client";
 
-const PostForm: React.FC = () => {
+import React from "react";
+import styles from "./post-form.module.scss";
+import { addPost } from "@/lib/action";
+
+const PostForm: React.FC = ({ userId }) => {
   return (
-    <form action="" className={styles.container}>
+    <form action={addPost} className={styles.container}>
       <h1>Add New Post</h1>
-      <input type="hidden" name="userId" />
+      <input type="hidden" name="userId" value={userId} />
       <input type="text" name="title" placeholder="Title" />
-      <input type="text" name="slug" placeholder="Slug" />
-      <input type="text" name="img" placeholder="Image src" />
-      <textarea name="description" placeholder="  Description" rows={10} />
+      <input type="text" name="slug" placeholder="slug" />
+      <input type="text" name="img" placeholder="img" />
+      <textarea name="description" placeholder="description" rows={10} />
       <button>Add</button>
     </form>
   );
---
+++ src/lib/action.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { connectToDb } from "./utils";
+import { Post } from "./models";
+import { revalidatePath } from "next/cache";
+
+export const addPost = async (formData) => {
+  const { description, slug, userId, img, title } =
+    Object.fromEntries(formData);
+  try {
+    connectToDb();
+    const newPost = new Post({
+      description,
+      slug,
+      userId,
+      img,
+      title,
+    });
+    await newPost.save();
+    revalidatePath("/blog");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Post not added");
+  }
+};
+
+export const deletePost = async (formData) => {
+  const { id } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+
+    await Post.findByIdAndDelete(id);
+    revalidatePath("/blog");
+    revalidatePath("/admin");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
+};
