5f52c023221808919e62337e74ab47ad2b08bc87
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2024-02-20 23:39:24 +0300

    fix: убирает приватность у блога

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -18,19 +18,12 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const user = auth?.user;
       const isOnAdminPanel = nextUrl?.pathname.startsWith(PAGE_ROUTES.admin);
-      const isOnBlogPage = nextUrl?.pathname.startsWith(PAGE_ROUTES.blog);
       const isOnLoginPage = nextUrl?.pathname.startsWith(PAGE_ROUTES.login);
       const isOnProfilePage = nextUrl?.pathname.startsWith(PAGE_ROUTES.profile);
 
       // ONLY ADMIN CAN REACH THE ADMIN DASHBOARD
 
       if (isOnAdminPanel && !user?.isAdmin) {
-        return false;
-      }
-
-      // ONLY AUTHENTICATED USERS CAN REACH THE BLOG PAGE
-
-      if (isOnBlogPage && !user) {
         return false;
       }
 
