66f5832712e889746e6291ec4924ac7d596fe594
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2024-02-23 20:43:48 +0300

    feat: блокирует форму входа при загрузке

--- src/components/login-form/LoginForm.tsx
+++ src/components/login-form/LoginForm.tsx
@@ -5,16 +5,29 @@ import Link from "next/link";
 import { login } from "@/lib/action";
 import styles from "./login-form.module.scss";
 import { PAGE_ROUTES } from "@/lib/helpers/const";
+import ButtonForm from "../button-form/ButtonForm";
+import useFormPending from "@/hooks/useFormPending";
 
 const LoginForm: React.FC = () => {
   const [state, formAction] = useFormState(login, undefined);
+  const { isPending, setIsPending } = useFormPending();
 
   return (
     <div className={styles.container}>
       <form action={formAction}>
-        <input type="email" name="email" placeholder="email" />
-        <input type="password" name="password" placeholder="password" />
-        <button>Login</button>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          disabled={isPending}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          disabled={isPending}
+        />
+        <ButtonForm setIsPending={setIsPending}>Login</ButtonForm>
         {state?.error}
         <Link href={PAGE_ROUTES.register}>
           {"Don't have an account?"} <b>Register</b>
