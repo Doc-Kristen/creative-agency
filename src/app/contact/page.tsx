34b51700ba78eef25dc860eb38549fd900281534
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2024-02-27 19:33:57 +0300

    feat: реализует анимацию на странице контактов

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
 import Form from "@/components/form/Form";
 import styles from "./contact.module.scss";
+import AnimatedLottie from "@/components/animated-lottie/AnimatedLottie";
+import animationData from "../../../public/lottie/contact-animation.json";
 
 export const metadata = {
   title: "Contact",
@@ -10,8 +11,8 @@ export const metadata = {
 const ContactPage: React.FC = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image src="/img/contact.png" alt="" fill className={styles.img} />
+      <div className={styles.imageContainer}>
+        <AnimatedLottie animationData={animationData} />
       </div>
       <div className={styles.formContainer}>
         <Form />
